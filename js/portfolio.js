e0cc207166c3f091af51edeb0a94b24aa6edc3c0
Alisa <alisa.luoma@hotmail.fi>
2023-05-17 13:39:09 +0300

    new project

--- js/portfolio.js
+++ js/portfolio.js
@@ -21,12 +21,23 @@ var accountURL = "https://github.com/Sisarus/"
 // portfolioProjects
 
 function fillData() {
-    let data = new PortfolioProject (
+
+    let  data = new PortfolioProject (
+        "ReactOpedia",
+        "In this app we test how React and Redux work together.",
+        "",
+        "react-redux",
+        "reduxOpedia.png",
+        ['frontend', 'react', 'redux', 'bootstap']
+    )
+    listProjects.push(data);
+    
+    data = new PortfolioProject (
         "Tindog",
         "Bootstrap practice. Single page for Dogs looking for new home.",
         "https://sisarus.github.io/Bootstrap-Tindog/",
         "Bootstrap-Tindog",
-        "img/portfolio_images/tindog-image.png",
+        "tindog-image.png",
         ['frontend', 'bootstap']
     )
     listProjects.push(data);
@@ -36,7 +47,7 @@ function fillData() {
         "I completed this exercise by only looking at the image and following the given styles. More information in project.",
         "https://sisarus.github.io/css-qrcode/",
         "qr-code-component-main",
-        "img/portfolio_images/learning.png",
+        "learning.png",
         ['frontend', 'bootstap']
     )
     listProjects.push(data);
@@ -46,7 +57,7 @@ function fillData() {
         "Play drums by pressing or using the keyboard.",
         "https://sisarus.github.io/js-drum/",
         "js-drum",
-        "img/portfolio_images/learning_1.png",
+        "learning_1.png",
         ['javascript']
     )
     listProjects.push(data);
@@ -56,7 +67,7 @@ function fillData() {
         "I have developed a small game using jQuery.",
         "https://sisarus.github.io/jQuery-Simons-Game/",
         "jQuery-Simons-Game",
-        "img/portfolio_images/learning_2.png",
+        "learning_2.png",
         ['jquery']
     )
     listProjects.push(data);
@@ -66,7 +77,7 @@ function fillData() {
         "I have implemented a newsletter subscription using Node.js and Mailchimp. However, since Heroku is no longer free, this feature is not currently available.",
         "https://sisarus.github.io/css-qrcode/",
         "qr-code-component-main",
-        "img/portfolio_images/Newletter.png",
+        "Newletter.png",
         ['frontend', 'bootstap']
     )
     listProjects.push(data);
@@ -76,7 +87,7 @@ function fillData() {
         "This is a Node.js project where users can input messages that are displayed and can also be deleted.",
         "https://sisarus.github.io/css-qrcode/",
         "Nodejs-toDoList",
-        "img/portfolio_images/nodejs-todolist.png",
+        "nodejs-todolist.png",
         ['node', 'express', 'rest']
     )
     listProjects.push(data);
@@ -86,7 +97,7 @@ function fillData() {
         "This is a Node.js project where users can input messages that are displayed and can also be deleted. Using NestJS",
         "",
         "nestjs-api-todo",
-        "img/backend.jpg",
+        "backend.jpg",
         ['backend', 'node', 'postgresql', 'docker', 'jwt' , 'rest']
     )
     listProjects.push(data);
@@ -96,7 +107,7 @@ function fillData() {
         "Simple backend for notes.",
         "",
         "Nodejs-todoList",
-        "img/backend.jpg",
+        "backend.jpg",
         ['backend', 'node', 'express', 'rest']
     )
     listProjects.push(data);
@@ -106,7 +117,7 @@ function fillData() {
         "Single page React app that read and saves data to json. States are stored bu Redux.",
         "",
         "react-redux-db",
-        "img/backend.jpg",
+        "backend.jpg",
         ['frontend', 'react', 'redux', 'bootstrap', 'rest']
     )
     listProjects.push(data);
@@ -116,7 +127,7 @@ function fillData() {
         "IBM is hosting a React Subject App Challenge that presents an opportunity for participants to showcase their skills in developing React applications. Single page web app.",
         "",
         "Budget-app",
-        "img/portfolio_images/budject_app01.png",
+        "budject_app01.png",
         ['frontend', 'react', 'bootstrap']
     )
     listProjects.push(data);
@@ -126,7 +137,7 @@ function fillData() {
         "This is a simple application for managing fruits and users using MongoDB as the database. It allows users to add, update, and delete fruits, as well as associate fruits with users. Run from shell. This only create base database for testing.",
         "",
         "fruits_database",
-        "img/backend.jpg",
+        "backend.jpg",
         ['backend', 'mongodb', 'node']
     )
     listProjects.push(data);
@@ -136,7 +147,7 @@ function fillData() {
         "This is a web application that provides user authentication and security features. Users can register, log in, and submit secrets. Secrets are visible to authenticated users only.",
         "",
         "auth-security",
-        "img/portfolio_images/secrets_01.png",
+        "secrets_01.png",
         ['backend', 'frontend', 'mongodb', 'node', 'ejs', 'session', 'mongoose', 'passportjs', 'google-authentication', 'express']
     )
     listProjects.push(data);
@@ -147,7 +158,7 @@ let cards = '';
 
 function makeCards(project) {
     cards += '<div class="col-md-3"><div class="card mt-3">';
-    cards += '<img class="card-img" src="' + project._image +'" alt="project image" />';
+    cards += '<img class="card-img" src="img/portfolio_images/' + project._image +'" alt="project image" />';
     cards += '<div class="bg-secondary text-center text-white p-1 mt-n3" disabled>';
     if(project._tags.length !== 0) {
         let tagNumber = 0;
